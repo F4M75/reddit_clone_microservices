514e9a0f820fa5a6a7209748fad575afd5cd75c2
Famenontsoa RALAHADY <famenontsoaralahady@gmail.com>
2026-02-15 23:59:19 +0300

    feat: add basic authorization

---
+++ api_gateway/src/jwt/index.ts
@@ -0,0 +1,4 @@
+export { JwtStrategy } from './jwt.strategy';
+export { JwtAuthGuard } from './jwt-auth.guard';
+export { Public } from './public.decorator';
+export { CurrentUser } from './current-user.decorator';
