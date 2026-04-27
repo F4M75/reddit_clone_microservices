de4d091b9ddd8f2938c05a7777acd39183a6a81f
Famenontsoa RALAHADY <famenontsoaralahady@gmail.com>
2026-04-27 22:32:40 +0300

    refactor: fix dto of update subreddit

--- shared-service-contract/src/dto/subreddit/update-subreddit.dto.ts
+++ shared-service-contract/src/dto/subreddit/update-subreddit.dto.ts
@@ -1,7 +1,20 @@
-import { IsString } from "class-validator";
+import { IsEnum, IsOptional, IsString } from "class-validator";
 import { CreateSubredditDto } from "./create-subreddit.dto";
+import { SubredditType } from "../../enum";
 
-export class UpdateSubredditDto extends CreateSubredditDto {
+export class UpdateSubredditDto {
   @IsString()
   id: string;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @IsString()
+  @IsOptional()
+  topic?: string;
+
+  @IsEnum(SubredditType)
+  @IsOptional()
+  type?: SubredditType;
 }
